e38b1cc46bcdba534c7570df0ac337311a381b9f
adselvar <adhita94@gmail.com>
2020-04-06 18:03:41 -0700

    fix smoktest script

--- test/kind-smoketest.sh
+++ test/kind-smoketest.sh
@@ -2,5 +2,5 @@
 
 export KFX_VERSION=v0.1.0-alpha
 export OPSYS=linux
-curl hhttp://storage.googleapis.com/kfx-releases/${KFX_VERSION}/${OPSYS}/kfx > kfx && chmod +x kfx
+curl http://storage.googleapis.com/kfx-releases/${KFX_VERSION}/${OPSYS}/kfx > kfx && chmod +x kfx
 ./kfx install kf
