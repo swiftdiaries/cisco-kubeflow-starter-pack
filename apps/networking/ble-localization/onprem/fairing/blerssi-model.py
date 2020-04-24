54e3469c25d435f3fd2c64bee8170a2a287a694b
Raviteja <samba.ravi999@gmail.com>
2020-04-24 05:01:52 +0000

    Deleted duplicate model files

--- apps/networking/ble-localization/onprem/fairing/blerssi-model.py
+++ apps/networking/ble-localization/onprem/fairing/blerssi-model.py
@@ -55,7 +55,7 @@ df_full = pd.read_csv('/opt/iBeacon_RSSI_Labeled.csv') #Labeled dataset
 
 # Input Data Preprocessing 
 df_full = df_full.drop(['date'],axis = 1)
-df_full[FEATURES] = (df_full[FEATURES]-df_full[FEATURES].mean())/df_full[FEATURES].std()
+df_full[FEATURES] = (df_full[FEATURES])/(-200)
 
 #Output Data Preprocessing
 dict = {'O02': 0,'P01': 1,'P02': 2,'R01': 3,'R02': 4,'S01': 5,'S02': 6,'T01': 7,'U02': 8,'U01': 9,'J03': 10,'K03': 11,'L03': 12,'M03': 13,'N03': 14,'O03': 15,'P03': 16,'Q03': 17,'R03': 18,'S03': 19,'T03': 20,'U03': 21,'U04': 22,'T04': 23,'S04': 24,'R04': 25,'Q04': 26,'P04': 27,'O04': 28,'N04': 29,'M04': 30,'L04': 31,'K04': 32,'J04': 33,'I04': 34,'I05': 35,'J05': 36,'K05': 37,'L05': 38,'M05': 39,'N05': 40,'O05': 41,'P05': 42,'Q05': 43,'R05': 44,'S05': 45,'T05': 46,'U05': 47,'S06': 48,'R06': 49,'Q06': 50,'P06': 51,'O06': 52,'N06': 53,'M06': 54,'L06': 55,'K06': 56,'J06': 57,'I06': 58,'F08': 59,'J02': 60,'J07': 61,'I07': 62,'I10': 63,'J10': 64,'D15': 65,'E15': 66,'G15': 67,'J15': 68,'L15': 69,'R15': 70,'T15': 71,'W15': 72,'I08': 73,'I03': 74,'J08': 75,'I01': 76,'I02': 77,'J01': 78,'K01': 79,'K02': 80,'L01': 81,'L02': 82,'M01': 83,'M02': 84,'N01': 85,'N02': 86,'O01': 87,'I09': 88,'D14': 89,'D13': 90,'K07': 91,'K08': 92,'N15': 93,'P15': 94,'I15': 95,'S15': 96,'U15': 97,'V15': 98,'S07': 99,'S08': 100,'L09': 101,'L08': 102,'Q02': 103,'Q01': 104}
@@ -74,7 +74,6 @@ def formatFeatures(features):
 
     for i in range(0, numColumns):
         formattedFeatures["b"+str(3001+i)] = features[:, i]
-
     return formattedFeatures
 
 trainingFeatures = formatFeatures(x1)
